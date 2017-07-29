d67906fdf1bab47f4dc023ee12fa3036790f49ff
HuRui <1194547767@qq.com>
2017-07-29 23:54:03 +0800

    ng-if选项卡

---
+++ 0504/vue3/day5/main.js
@@ -0,0 +1,22 @@
+/*var angular = require("angular");
+var $ = require("jquery");
+console.log($)
+$("p").css("color","red")
+var app = angular.module("demo", []);
+var indexCtrl = require("./controller/indexCtrl.js");
+indexCtrl(app)*/
+class obj {
+	constructor(name){
+		this.name = name
+	};
+	test(){
+		console.log("test")
+	}
+}
+let a = new obj("abc")
+
+var str = `
+	abc
+	cbd
+	def
+`;
